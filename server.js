741efc96ff6795e8c04a88de20d38ce1ec725fee
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-02 11:17:56 -0700

    made changes to tags routes

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const routes = require('./routes');
 
@@ -13,6 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+});
+
